e21a5390974322cb92d0d07d44da43744c1ad845
stefankotupov <stefan_kotupov@abv.bg>
2016-05-22 17:54:13 +0300

    gzs

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int n = int.Parse(Console.ReadLine());
+            int a = 1;
+          
+
+            for (int i = 2; i <= n - 1; i++)
+            {
+                a = n % i;
+                if (a == 0)
+                {
+                    
+                    break;
+                }
+               
+            }
+            if (a==0 || n==1 || n==0 || n<0)
+            {
+                Console.WriteLine("Not Prime");
+            }
+            else                 {
+                    Console.WriteLine("Prime");
+                }
+
+            
+            }
+        }
+    }
+
+
+    
+
